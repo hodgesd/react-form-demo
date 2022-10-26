55c0a2a99a4a907eed8df855f9b3d262d43c2ad3
Derrick Hodges <hodgesd@gmail.com>
2022-10-26 00:36:04 -0500

    feat: :sparkles: uncontrolled example
    changed to uncontrolled for clarity

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,14 +1,29 @@
 import PropTypes from "prop-types";
-import Input from "./Input";
 
 export default function Form({ fields }) {
+  const handleSubmit = e => {
+    e.preventDefault();
+    const form = e.target;
+    console.log('email', form.email, form.elements.email);
+    console.log('password', form.password, form.elements.password);
+  }
   return (
-    <form>
-      {fields.map((field) => (
-        <Input key={field.label} label={field.label} type={field.type} />
-      ))}
-
-      {/* TODO: Add a submit button! */}
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="email">Email address</label>
+        <input
+          id="email"
+          name="email"
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <input
+          id="password"
+          name="password"
+        />
+      </div>
+      <button type="submit">Submit</button>
     </form>
   );
 }
